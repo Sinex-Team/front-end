3880a1adb7fae2ff7635b1d62dc6299300f1e8a6
Yaryna Kos <117029435+yarynakos@users.noreply.github.com>
2023-05-03 18:35:37 +0300

    created view login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
-  { path: 'reg', component: RegistrationPageComponent }
+  { path: 'reg', component: RegistrationPageComponent },
+  {path: 'login', component: LoginPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationPageComponent
+    RegistrationPageComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
